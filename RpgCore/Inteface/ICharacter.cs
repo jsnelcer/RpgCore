90df26c87549608198161fd327f4c4f3f5e99b20
Jozef Šnelcer <jsnelcer@hotmail.com>
2019-07-18 20:38:56 +0200

    remove characters

--- RpgCore/Inteface/ICharacter.cs
+++ RpgCore/Inteface/ICharacter.cs
@@ -1,10 +1,8 @@
-﻿using System.Collections.Generic;
-
-namespace RpgCore.Inteface
+﻿namespace RpgCore.Inteface
 {
-    public interface ICharacter : IEntity
+    public interface ICharacter
     {
-        IStorage<IItem> Inventory { get; }
-        List<IEntity> LookAround();
+        string Name { get; }
+        string Description { get; }
     }
 }
